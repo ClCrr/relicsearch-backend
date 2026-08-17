b93511d0e9bed0d10d48ecb1daedfc41995d1683
Claudio Carrera <claudiocarrerah@gmail.com>
2026-08-17 08:21:25 -0500

    main project stuff

---
+++ src/test/java/com/univo/relicsearch_backend/RelicsearchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univo.relicsearch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelicsearchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
